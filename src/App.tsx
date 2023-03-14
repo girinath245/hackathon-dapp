fdcb11d4f82e8e6bfb5c3aadbe1308fc0ae82648
Girinath <girinath2452001@gmail.com>
2023-03-14 06:49:52 +0530

    Started Implementing Dapp.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import '@polkadot/api-augment';
 
 import { ApiPromise } from '@polkadot/api';
+import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
 import { WsProvider } from '@polkadot/rpc-provider';
-import { options } from '@astar-network/astar-api';
 
 function App() {
-  const connect = async () => {
-    // const provider = new WsProvider('ws://localhost:9944');
-    // OR
-    const provider = new WsProvider('wss://shiden.api.onfinality.io/public-ws');
-    const api = new ApiPromise(options({ provider }));
-    await api.isReady;
+  const [accounts, setAccount] = useState([]);
+  const [selectedAcccount, setSelectedAccount] = useState();
 
-    // Use the api
-    // For example:
+  const setup = async () => {
+    const wsProvider = new WsProvider('wss://shiden.api.onfinality.io/public-ws');
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    console.log(api.genesisHash.toHex());
     console.log((await api.rpc.system.properties()).toHuman());
-  }
+  };
+
+  const handleConnection = async () => {
+    const extensions = await web3Enable('management-app');
+    if (extensions.length === 0) {
+      return;
+    }
+    const allAccounts = await web3Accounts();
+    console.log(allAccounts);
+  };
+
+  useEffect(() => {
+    setup();
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={connect}> Connect</button>
+        <button onClick={handleConnection}> Connect To Wallet </button>
       </header>
     </div>
   );
